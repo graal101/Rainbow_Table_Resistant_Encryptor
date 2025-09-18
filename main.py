2fe9e96c1368eae86e028eed28a9896cc5d33fc4
Andrew Andrey <graal101@mail.ru>
2025-09-18 18:15:10 +0700

    create form

--- main.py
+++ main.py
@@ -2,11 +2,26 @@
 # -*- coding: utf-8 -*-
 #
 #  main.py
+import sys
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ui/main_window.ui', self)
+
+        # Подключаем сигналы и слоты
+        # self.pushButton.clicked.connect(self.on_button_click)
+
+    # def on_button_click(self):
+        # print("Кнопка нажата!") 
 
 
-def main(args):
-    return 0
 
 if __name__ == '__main__':
-    import sys
-    sys.exit(main(sys.argv))
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+    
